eb4da301ad782ce4ad2e6a749ff90f452750785e
Lucas Costa <luchtml5@gmail.com>
2023-01-16 11:32:15 -0300

    select

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const App = () => {
-  const [textarea, setTextarea] = React.useState('');
+  const [select, setSelect] = React.useState('');
   return (
     <form>
-      <textarea
-        rows="10"
-        value={textarea}
-        onChange={({ target }) => {
-          setTextarea(target.value);
-        }}
-      />
-      {textarea}
+      <select id="produtos" value={select} onChange={({target}) => {
+        setSelect(target.value)
+      }}>
+        <option disabled value="">Selecione</option>
+        <option value="notebook">Notebook</option>
+        <option value="smartphone">Smartphone</option>
+        <option value="tablet">Tablet</option>
+      </select>
+      {select}
     </form>
   );
 };
