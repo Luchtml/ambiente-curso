4823a519e4548495f0c68810ac6279564897ef1b
Lucas Costa <luchtml5@gmail.com>
2023-01-18 15:25:21 -0300

    validação

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React from 'react';
-import Checkbox from './Form/Checkbox';
-import Input from './Form/Input';
-import Radio from './Form/Radio';
-import Select from './Form/Select';
+import React from 'react'
 
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
-  const [frutas, setFrutas] = React.useState('');
-  const [linguagens, setLinguagens] = React.useState([]);
-
+  const [cep, setCep] = React.useState('')
   return (
     <form>
-      <h2>Linguagens</h2>
-      <Checkbox
-        options={['JavaScript', 'PHP', 'Ruby']}
-        value={linguagens}
-        setValue={setLinguagens}
-      />
-      <h2>Cores</h2>
-      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
-      <h2>Frutas</h2>
-      <Radio
-        options={['Limão', 'Laranja', 'Uva']}
-        value={frutas}
-        setValue={setFrutas}
-      />
-      <Select
-        options={['smartphone', 'tablet']}
-        value={produto}
-        setValue={setProduto}
-      />
-      <Input label="nome" id="nome" value={nome} setValue={setNome} required />
-      <Input label="email" id="email" value={email} setValue={setEmail} />
-      <button>Enviar</button>
+      
     </form>
-  );
-};
+  )
+}
 
-export default App;
+export default App
