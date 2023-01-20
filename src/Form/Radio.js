4a089bd8583f7646d75cba14bad328e993da148d
Lucas Costa <luchtml5@gmail.com>
2023-01-20 11:54:36 -0300

    exercicio radio

--- src/Form/Radio.js
+++ src/Form/Radio.js
@@ -1,21 +1,33 @@
 import React from 'react';
 
-const Radio = ({ options, value, setValue, ...props }) => {
+const Radio = ({ pergunta, id, options, onChange, value, active }) => {
+
+  if(active === false ) return null
   return (
-    <>
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
       {options.map((option) => (
-        <label key={option}>
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
           <input
+            id={id}
             type="radio"
-            value={option}
             checked={value === option}
-            onChange={({ target }) => setValue(target.value)}
-            {...props}
+            value={option}
+            onChange={onChange}
           />
           {option}
         </label>
       ))}
-    </>
+    </fieldset>
   );
 };
 
