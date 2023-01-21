8542688f6245ff8678d13a37558fa77fe88138ae
Lucas Costa <luchtml5@gmail.com>
2023-01-21 12:47:13 -0300

    framworks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Card bg="dark" text="white" className='m-5' style={{maxWidth: '18rem'}}>
+      <Card.Header>Notebook</Card.Header>
+      <Card.Body>
+        <Card.Title>R$ 2500</Card.Title>
+        <Card.Text>R$ 2500</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default App;
