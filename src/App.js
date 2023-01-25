250f86073ace22140a76c50e87697647916225f9
Lucas Costa <luchtml5@gmail.com>
2023-01-25 17:34:52 -0300

    initiit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Contato from './Components/Contato';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Produto from './Components/Produto';
-import Produtos from './Components/Produtos';
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Produtos />} />
-            <Route path="produto/:id" element={<Produto />} />
-            <Route path="contato" element={<Contato />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
