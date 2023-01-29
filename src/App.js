45f617de3271a7816a84e1fbb4a5f87863d6e8a0
Lucas Costa <luchtml5@gmail.com>
2023-01-29 11:46:50 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Exemplo from "./Exemplo";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+function reducer(state, action) {
+
+  switch(action) {
+    case 'aumentar':
+    return state + 1
+    case 'diminuir':
+      return state -1
+    default:
+      throw new Error()
+  }
+
+
+//   if (action === "aumentar") {
+//     return state + 1;
+//   }
+//   if (action === "diminuir") {
+//     return state - 1;
+//   }
+//   return new Error("Erro, action dont exist");
 }
 
-export default App
+const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
+  return (
+    <div>
+      <button onClick={() => dispatch("aumentar")}>Mais</button>
+      <button onClick={() => dispatch("diminuir")}>Menos</button>
+      <p>{state}</p>
+      <Exemplo />
+    </div>
+  );
+};
+
+export default App;
